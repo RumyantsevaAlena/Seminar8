9213dc8de5f1e53d97a375c3fc5f8b9b4f98de75
RumyantsevaAlena <kaluga1993@mail.ru>
2023-02-13 17:14:08 +0300

    решение задач к 8 семинару

---
+++ task0001/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+//которая упорядочит по убыванию элементы каждой строки двумерного массива.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//В итоге получается вот такой массив:
+//7 4 2 1
+//9 5 3 2
+//8 4 4 2
+
+void FillMatrixRandomNumbers(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void OrderArrayLines(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+Console.WriteLine($"\n Введите размер массива m x n и диапазон случайных значений");
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int range = InputNumbers("Введите диапазон: от 1 до ");
+int[,] numbers = new int[m, n];
+FillMatrixRandomNumbers(numbers);
+PrintMatrix(numbers);
+Console.WriteLine($"\n Отсортированный массив: ");
+OrderArrayLines(numbers);
+PrintMatrix(numbers);
